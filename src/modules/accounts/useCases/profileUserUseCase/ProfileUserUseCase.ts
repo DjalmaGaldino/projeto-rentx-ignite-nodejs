25d4abc7d5000d02963710ad57b75f67816cecd0
Djalma Galdino <galdino.dj@gmail.com>
2022-10-27 20:43:08 -0300

    configurating deployment class

--- src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUserUseCase/ProfileUserUseCase.ts
@@ -1,24 +1,19 @@
-import { User } from "@modules/accounts/infra/typeorm/entities/User";
-import { UserMap } from "@modules/accounts/mapper/UserMap";
-import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-
 import { inject, injectable } from "tsyringe";
-
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { UserMap } from "@modules/accounts/mapper/UserMap";
+import { IUserResponseDTO } from "@modules/accounts/dtos/IUserResponseDTO ";
 
 @injectable()
 class ProfileUserUseCase {
-
   constructor(
     @inject("UsersRepository")
     private usersRepository: IUsersRepository
   ) {}
-
-  async execute(id: string): Promise<User> {
+  async execute(id: string): Promise<IUserResponseDTO> {
     const user = await this.usersRepository.findById(id);
-    
+
     return UserMap.toDTO(user);
   }
-
 }
 
-export { ProfileUserUseCase }
+export { ProfileUserUseCase };
---
+++ src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,16 @@
+import { container } from "tsyringe";
+import { IMailProvider } from "./IMailProvider";
+import { EtherealMailProvider } from "./implementation/EtherealMailProvider";
+import { SESMailProvider } from "./implementation/SESMailProvider";
+
+
+
+const mailProvider = {
+  local: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider)
+}
+
+container.registerInstance<IMailProvider>(
+  "MailProvider",
+  mailProvider[process.env.MAIL_PROVIDER]
+);

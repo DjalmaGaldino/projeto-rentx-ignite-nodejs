5e153714d3949afda67ccb2f71cdcee77d565bd7
Djalma Galdino <galdino.dj@gmail.com>
2022-10-30 21:30:49 -0300

    final commit

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,42 +1,62 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from 'express';
-import "express-async-errors"
-import createConnection  from "@shared/infra/typeorm";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
-import "@shared/container"
+import "@shared/container";
+import upload from "@config/upload";
+import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
+import createConnection from "@shared/infra/typeorm";
 
-import { router } from './routes/Index';
-import swaggerUi from 'swagger-ui-express';
-import swaggerFile from '../../../swagger.json';
-import { AppError } from '@shared/errors/AppError';
-import upload from '@config/upload';
-
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes/Index";
 
 createConnection();
 const app = express();
 
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile) );
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
-app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
+app.use(cors());
+app.use(router);
 
-app.use(router)
+app.use(Sentry.Handlers.errorHandler());
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if(err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message
-    })
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
   }
+);
 
-  return response.status(500).json({
-    status: "error",
-    message: `Internal server error :: ${err.message}`
-  })
-})
-
-export { app }
+export { app };
